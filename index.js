1cc081e305069f2910e4655b91d72abf713eb826
astro0151 <gulbahar0151@gmail.com>
2021-11-12 15:16:46 +0530

    first commit,html css added...

---
+++ index.js
@@ -0,0 +1,8 @@
+let dateObject = new Date();
+let date = dateObject.getDate();
+console.log(date);
+console.log(dateObject.getDay());
+console.log(dateObject.getFullYear());
+console.log(dateObject.getHours());
+console.log(dateObject.getMinutes());
+console.log(dateObject);
